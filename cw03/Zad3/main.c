9572af71bb550e42451a43592690ae55d928270f
Adam Niemiec <72918433+Deutscher862@users.noreply.github.com>
2021-04-05 23:16:15 +0200

    Add files via upload

--- cw03/Zad3/main.c
+++ cw03/Zad3/main.c
@@ -1,54 +1,72 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/wait.h>
 #include <sys/types.h>
+#include <unistd.h>
+#include <string.h>
 
-void search_for_files(int current_depth, int max_depth, char* look_for);
+void searchForFiles(int current_depth, int max_depth, char* look_for_file, char* new_directory);
+
+char* getOutputFromCommand(char* command);
 
 int main(int argc, char **argv){
-    if(argc != 4){
-        printf("No nie");
-        exit(-1);
-    }
-    
-    char* first_cat = argv[1];
-    char* look_for = argv[2];
-    int max_depth = argv[3];
+    if(argc != 4)
+        exit(-1);   
+    char* first_directory = argv[1];
+    char* look_for_file = argv[2];
+    int max_depth = atoi(argv[3]);
+    searchForFiles(0, max_depth, look_for_file, first_directory);
+    return 0;
 }
 
-void search_for_files(int current_depth, int max_depth, char* look_for){
+void searchForFiles(int current_depth, int max_depth, char* look_for_file, char* new_directory){
+    printf("\nEntering new catalog: \n");
+    printf("%s\n", new_directory);
+    printf("My PID: %d\nFound files:\n", (int)getpid());
+    
     //print all text files with given string
-    printf("Entering new catalog: \n");
-    system("pwd");
-    printf("My PID: %d\n Found these files: ", (int)getpid());
     char mess[100];
-    sprintf(mess, "ls *.txt | grep %s", look_for);
+    sprintf(mess, "find %s -maxdepth 1 -type f -name '*.txt' | grep %s", new_directory, look_for_file);
     system(mess);
 
     //get number of directories in current directory
-    FILE* dir_counter = popen("ls -l | grep -c ^d", "r");
-    if (dir_counter == NULL) {
-        printf("Failed to run command\n" );
-        exit(-1);
-    }
-    int dir_count;
-    char buffer[10];
-    while (fgets(buffer, sizeof(buffer), dir_counter) != NULL)
-        dir_count = atoi(buffer);
-    
-    pclose(dir_counter);
+    char mess2[100];
+    sprintf(mess2, "ls %s -l | grep -c ^d", new_directory);
+    int dir_counter = atoi(getOutputFromCommand(mess2));
 
     if(current_depth == max_depth || dir_counter == 0)
-        return;
-    
-    //loop recursivly for directories in current directory
-    for(int i = 0; i < dir_count; i++){
-        pid_t new_child = fork();
-        char next_directory;
+        exit(1);
 
-        // next_directory -------------------------------------------------
-        if(new_child == 0) 
-            search_for_files(current_depth + 1, max_depth, look_for);
+    //loop recursivly for directories in current directory
+    for(int i = 0; i < dir_counter; i++){
+        pid_t new_child = fork();
+        if(new_child == 0){
+            char mess3[100];
+            sprintf(mess3, "ls %s -l | grep ^d | head -%d | tail -1 | awk '{print $9}'",new_directory, i+1);
+            char* next_directory = new_directory;
+            sprintf(next_directory, "%s/%s", new_directory, getOutputFromCommand(mess3));
+            searchForFiles(current_depth + 1, max_depth, look_for_file, next_directory);
+            exit(1);
+        }
     }
-    for(int i = 0; i < dir_count; i++)
+    for(int i = 0; i < dir_counter; i++)
         wait(NULL);
+    exit(1);
+}
+
+char* getOutputFromCommand(char* command){
+    //return char* with terminal command output
+    FILE* output_file = popen(command, "r");
+    if (output_file == NULL) {
+        printf("Failed to run command\n");
+        exit(-1);
+    }
+    else{
+        char buffer[100];
+        fgets(buffer, sizeof(buffer), output_file);
+        buffer[strcspn(buffer, "\n")] = 0;
+        char *result = buffer;
+        pclose(output_file);
+        return result;
+    }
 }
