03310d2d0d223cf7c41d40d04e27af38b16bd47f
Adam Niemiec <72918433+Deutscher862@users.noreply.github.com>
2021-04-08 22:31:23 +0200

    Add files via upload

---
+++ cw04/Zad1/exec.c
@@ -0,0 +1,20 @@
+#define SIGSUR1 10
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <string.h>
+
+int main(int argc, char **argv){
+    char* sig_type = argv[1];
+
+    if (strcmp(sig_type, "pending") != 0){
+        raise(SIGUSR1);
+    }
+    if (strcmp(sig_type, "mask") == 0 || strcmp(sig_type, "pending") == 0){
+        sigset_t signal_set;
+        sigpending(&signal_set);
+        printf("Signal %d pending\n", sigismember(&signal_set, SIGUSR1));
+    }
+    return 0;
+}
---
+++ cw04/Zad1/main.c
@@ -0,0 +1,66 @@
+#define SIGSUR1 10
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <string.h>
+
+void handler(int signum);
+
+void checkPending(char* sig_type, sigset_t signal_set);
+
+int main(int argc, char **argv){
+
+    if(argc == 1 || argc > 3)
+        exit(0);
+
+    char* sig_type = argv[1];
+
+    if(strcmp(sig_type, "ignore") == 0)
+        signal(SIGUSR1, SIG_IGN);
+
+    else if(strcmp(sig_type, "handler") == 0)
+        signal(SIGUSR1, handler);
+    
+    else if (strcmp(sig_type, "mask") == 0 || strcmp(sig_type, "pending") == 0){
+        sigset_t signal_set;
+        //init empty signal set
+        sigemptyset(&signal_set);
+        //add singal to set
+        sigaddset(&signal_set, SIGUSR1);
+        //set new mask set 
+        if (sigprocmask(SIG_BLOCK, &signal_set, NULL) < 0)
+            perror("Signal blocking failed");
+    }
+    raise(SIGUSR1);
+    sigset_t signal_set;
+
+    checkPending(sig_type, signal_set);
+
+    if (strcmp(argv[2], "exec") == 0)
+        execl("./exec", "./exec", sig_type, NULL);
+    else{
+        pid_t child_pid = fork();
+        if (child_pid == 0){
+            if (strcmp(sig_type, "pending") != 0)
+                raise(SIGUSR1);
+            checkPending(sig_type, signal_set);
+        }
+        exit(0);
+        wait(NULL);
+    }
+    return 0;
+}
+
+void handler(int signum){
+    printf("Signal received\n");
+}
+
+void checkPending(char* sig_type, sigset_t signal_set){
+    if (strcmp(sig_type, "mask") == 0 || strcmp(sig_type, "pending") == 0){
+        sigpending(&signal_set);
+        printf("Signal %d pending\n", sigismember(&signal_set, SIGUSR1));
+    }
+}
