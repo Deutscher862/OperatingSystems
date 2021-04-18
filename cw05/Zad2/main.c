98a275607cbf704c9b81abee5ee5bced08391153
Adam Niemiec <72918433+Deutscher862@users.noreply.github.com>
2021-04-18 19:47:20 +0200

    Add files via upload

--- cw05/Zad2/main.c
+++ cw05/Zad2/main.c
@@ -1,59 +1,29 @@
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-void print_output(FILE* file){
-    char line[100];
-    while(fgets(line, 100, file) != NULL){
-        printf("%s\n", line);
-    }
-}
-
-void print_ordered_by(char* mode){
-    FILE* file;
-    char* command;
-
-    if (strcmp(mode,"date") == 0) {
-        command = "echo | mail -f | tail +2 | head -n -1 | tac";
-    }
-    else if (strcmp(mode, "sender") == 0){
-        command = "echo | mail -f | tail +2 | head -n -1 | sort -k 2";
-    }
-    else
-        exit(1);
-
-    file = popen(command, "r");
-
-    if (file == NULL)
-        exit(1);
-
-    print_output(file);
-    pclose(file);
-}
-
-void send_email(char* address, char* subject, char* content){
-    FILE* file;
-    char command[100];
-
-    snprintf(command, sizeof(command), "echo %s | mail -s %s %s", content, subject, address);
-    printf("%s\n", command);
-    file = popen(command, "r");
-
-    if (file == NULL)
-        exit(1);
-    print_output(file);
-    pclose(file);
-}
-
-int main(int argc, char* argv[]){
+int main(int argc, char** argv){
     if (argc == 2){
-        print_ordered_by(argv[1]);
+        if (strcmp(argv[1], "date") == 0){
+            FILE* mail = popen("mail | sort -M", "w");
+            pclose(mail);
+        }
+        else if(strcmp(argv[1], "sender") == 0){
+            FILE* mail = popen("mail | sort -k 3", "w");
+            pclose(mail);
+        }
     }
-    else if(argc == 4){
-        send_email(argv[1], argv[2], argv[3]);
+    else if (argc == 4){
+        char* adress = argv[1];
+        char* subject = argv[2];
+        char* content = argv[3];
+        char command[100];
+        sprintf(command, "mail -s %s %s", subject, adress);
+        
+        FILE* mail = popen(command, "w");
+        fputs(content, mail);
+        pclose(mail);
+        printf("Mail sent succesfully\n");
     }
-    else{
-        printf("Wrong number of arguments!");
-        exit(1);
-    }
+    return 0;
 }
