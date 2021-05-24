22099ae1efebed465b6a8de9e8b03ece241d9aa5
Adam Niemiec <72918433+Deutscher862@users.noreply.github.com>
2021-05-24 16:19:26 +0200

    Add files via upload

--- cw09/Zad1/main.c
+++ cw09/Zad1/main.c
@@ -7,6 +7,7 @@
 #include <time.h>
 
 int santa_sleeping = 0;
+pthread_t* thread_ids;
 
 pthread_t reindeers_queue[9];
 int reindeers_back = 0;
@@ -58,16 +59,18 @@ void* santa(void* arg) {
         pthread_mutex_unlock(&mutex);
 
     }
-        pthread_exit((void *) 0);
+
+    //end other threads
+    for(int i = 1; i < 20; i++)
+        pthread_cancel(thread_ids[i]);
+
+    pthread_exit((void *) 0);
 }
 
 void* elf(void* arg) {
     pthread_t id = pthread_self();
     while(1) {
-        
-        if(gifts_delivered == 3)
-            break;
-        
+
         if(elves_with_problems == 3){
             for(int i = 0; i < 3; i++)
                 if(elves_queue[i] == id)
@@ -76,6 +79,7 @@ void* elf(void* arg) {
         
         usleep(getRandomTime(2000, 5000));
         pthread_mutex_lock(&mutex);
+
         if(elves_with_problems == 2 && santa_sleeping == 1){
             elves_queue[elves_with_problems] = id;
             elves_with_problems++;
@@ -103,9 +107,6 @@ void* reindeer(void* arg) {
         if(queue_id != -1 && reindeers_queue[queue_id] == id)
             continue;
 
-        if(gifts_delivered == 3)
-            break;
-
         usleep(getRandomTime(5000, 10000));
         pthread_mutex_lock(&mutex);
         queue_id = reindeers_back;
@@ -131,7 +132,7 @@ int main(int argc, char** argv) {
 
     pthread_mutex_init(&mutex, NULL);
 
-    pthread_t* thread_ids = (pthread_t*) calloc(20, sizeof(pthread_t));
+    thread_ids = (pthread_t*) calloc(20, sizeof(pthread_t));
 
     pthread_create(&thread_ids[0], NULL, santa, NULL);
     for(int i = 1; i < 11; i++) {
